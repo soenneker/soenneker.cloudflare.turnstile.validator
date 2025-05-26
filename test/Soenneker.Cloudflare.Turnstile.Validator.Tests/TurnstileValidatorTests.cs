10b5377056be6b74551dd2cb685242f073217a04
Jake Soenneker <jake@soeneker.com>
2025-05-26 23:38:37 +0000

    FluentAssertions -> AwesomeAssertions namespace

--- test/Soenneker.Cloudflare.Turnstile.Validator.Tests/TurnstileValidatorTests.cs
+++ test/Soenneker.Cloudflare.Turnstile.Validator.Tests/TurnstileValidatorTests.cs
@@ -1,5 +1,5 @@
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Soenneker.Cloudflare.Turnstile.Validator.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
